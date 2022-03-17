eccc9b6f76be30762ddb90984d7670e9735c300e
andre-bandeli <andre.lbandeli@gmail.com>
2022-03-17 02:39:00 -0300

    classe DAO funcionario

--- src/main/java/com/br/Estoque/estoque/EstoqueApplication.java
+++ src/main/java/com/br/Estoque/estoque/EstoqueApplication.java
@@ -1,5 +1,6 @@
 package com.br.Estoque.estoque;
 
+import com.br.Estoque.estoque.funcionario.DAO.FuncionarioDAO;
 import com.br.Estoque.estoque.funcionario.model.Funcionario;
 import com.br.Estoque.estoque.funcionario.servico.FuncionarioService;
 import org.springframework.boot.SpringApplication;
@@ -11,15 +12,13 @@ public class EstoqueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EstoqueApplication.class, args);
 
-		Funcionario funcionario = new Funcionario();
-		FuncionarioService service = null;
-		try {
-			funcionario.setNome("Teste");
-			service.saveFuncionario(funcionario);
-		}catch (Exception e) {
-			e.fillInStackTrace();
-		}
 
+		Funcionario funcionario = new Funcionario();
+		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+
+		funcionario.setId(1L);
+		funcionario.setNome("TESTE");
+		funcionarioDAO.criarFuncionario(funcionario);
 
 	}
 
---
+++ src/main/java/com/br/Estoque/estoque/factory/Factory.java
@@ -0,0 +1,37 @@
+package com.br.Estoque.estoque.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class Factory {
+
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/managerM";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+
+        }catch (ClassNotFoundException | SQLException exception) {
+            throw new RuntimeException("Erro: ", exception);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement pst) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException("Erro: ", exception);
+        }
+    }
+
+}
